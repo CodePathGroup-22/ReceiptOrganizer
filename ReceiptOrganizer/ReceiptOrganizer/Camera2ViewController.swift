61b7a0586d96e57daa4661095f4c3bc9c7e4eec3
Ann Wu <weian.wu824@hotmail.com>
2022-04-17 04:12:15 -0400

    user can stays logged in and can logout

---
+++ ReceiptOrganizer/ReceiptOrganizer/Camera2ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  Camera2ViewController.swift
+//  ReceiptOrganizer
+//
+//  Created by mac book pro on 4/17/22.
+//
+
+import UIKit
+import AlamofireImage
+import Parse
+
+class Camera2ViewController: UIViewController, UIImagePickerControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ReceiptOrganizer/ReceiptOrganizer/CategoriesViewController.swift
+++ ReceiptOrganizer/ReceiptOrganizer/CategoriesViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AlamofireImage
+import Parse
 
 class CategoriesViewController: UIViewController {
 
@@ -15,7 +17,17 @@ class CategoriesViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        
+        delegate.window?.rootViewController = loginViewController
+    }
+    
     /*
     // MARK: - Navigation
 
